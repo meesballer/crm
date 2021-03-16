b47bc0884e53d83845e7244fd34c2bbcf158f9c0
meesballer <meesbakker100@gmail.com>
2021-03-16 16:15:42 +0100

    Tests voor employee en company model gemaakt

--- tests/Feature/CompanyTest.php
+++ tests/Feature/CompanyTest.php
@@ -2,21 +2,56 @@
 
 namespace Tests\Feature;
 
+use App\Employee;
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class CompanyTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    use RefreshDatabase;
+    /** @test */
+    public function only_logged_in_users_can_see_company_list()
     {
-        $response = $this->get('/');
+        $response = $this->get('/companies')
+            ->assertRedirect('/login');
+    }
+    /** @test */
+    public function authenthicated_users_can_see_company_list()
+    {
+        $this->actingAs(factory(User::class)->create());
 
-        $response->assertStatus(200);
+        $response = $this->get('/companies')
+            ->assertOk();
+    }
+
+
+    /** @test */
+    public function company_can_be_added_through_form()
+    {
+        $this->actingAs(factory(User::class)->create());
+
+        $response = $this->post('/companies', $this->data());
+
+        $this->assertCount(1, Employee::all());
+
+    }
+
+    private function data()
+    {
+        return [
+            'name' => 'tescompany',
+            'email' => 'company@company.com',
+            'address' => 'boulevard 3',
+            'website'=>'test.com',
+            'user_id' => '1',
+        ];
+    }
+
+    /** @test */
+    public function a_name_is_required()
+    {
+
     }
 }
--- tests/Feature/EmployeeTest.php
+++ tests/Feature/EmployeeTest.php
@@ -2,21 +2,59 @@
 
 namespace Tests\Feature;
 
+use App\Employee;
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class EmployeeTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
+    use RefreshDatabase;
+    /** @test */
     public function only_logged_in_users_can_see_employees_list()
     {
       $response = $this->get('/employees')
           ->assertRedirect('/login');
     }
+    /** @test */
+    public function authenthicated_users_can_see_employees_list()
+    {
+        $this->actingAs(factory(User::class)->create());
+
+        $response = $this->get('/employees')
+        ->assertOk();
+    }
+
+
+    /** @test */
+    public function employee_can_be_added_through_form()
+    {
+        $this->actingAs(factory(User::class)->create());
+
+        $response = $this->post('/employees', $this->data());
+
+            $this->assertCount(1, Employee::all());
+
+    }
+
+    private function data()
+    {
+        return [
+            'firstname' => 'Test',
+            'lastname' => 'Employee',
+            'email' => 'test@employee.com',
+            'phone' => '0675382391',
+            'company_id' => '1',
+            'user_id' => '1',
+            'role_id' => '1',
+        ];
+    }
+
+    /** @test */
+public function a_firstname_is_required()
+{
+
+}
 
 }
